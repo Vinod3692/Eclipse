aa007b789ddc0952c0d9a942bdcd57a507745b95
Vinod Kalagi <vinodmk369@gmail.com>
2023-07-26 09:27:36 +0530

    Object Concept

---
+++ Prime/src/com/xworkz/prime/app/Ladoo.java
@@ -0,0 +1,12 @@
+package com.xworkz.prime.app;
+
+public class Ladoo {
+
+	@Override
+	public String toString() {
+		System.out.println("Running toString in Ladoo");
+		
+		return "Ladoo";
+	}
+	
+}
---
+++ Prime/src/com/xworkz/prime/runner/FinanceRunner.java
@@ -0,0 +1,74 @@
+package com.xworkz.prime.runner;
+
+import com.xworkz.prime.app.*;
+
+public class FinanceRunner {
+
+	public static void main(String[] args) {
+
+		System.out.println("Running main method in ALl Runner\n");
+
+		Object obj1 = new Finance();
+		obj1.toString();
+
+		Finance finance = new Finance();
+		finance.toString();
+
+		Object obj2 = new Ladoo();
+		obj2.toString();
+
+		Ladoo ladoo = new Ladoo();
+		ladoo.toString();
+		System.out.println();
+
+		Object obj3 = new Lake();
+		obj3.toString();
+
+		Lake lake = new Lake();
+		lake.toString();
+
+		Object obj4 = new Lotus();
+		obj4.toString();
+
+		Lotus lotus = new Lotus();
+		lotus.toString();
+
+		Object obj5 = new MLA();
+		obj5.toString();
+
+		MLA mla = new MLA();
+		mla.toString();
+
+		Object obj6 = new Money();
+		obj6.toString();
+
+		Money money = new Money();
+		money.toString();
+
+		Object obj7 = new President();
+		obj7.toString();
+
+		President president = new President();
+		president.toString();
+
+		Object obj8 = new PrimeMinister();
+		obj8.toString();
+
+		PrimeMinister pm = new PrimeMinister();
+		pm.toString();
+		System.out.println();
+
+		Object obj9 = new River();
+		obj9.toString();
+
+		River river = new River();
+		river.toString();
+
+		Object obj10 = new Security();
+		obj10.toString();
+
+		Security security = new Security();
+		security.toString();
+	}
+
+}
